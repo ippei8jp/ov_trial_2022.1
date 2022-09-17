87931144c3d4da37fe2a9d5076f3b4460fd576ed
いっぺーちゃん <48367499+ippei8jp@users.noreply.github.com>
2022-09-17 14:22:48 +0900

    画像ファイル追加

--- images/download.sh
+++ images/download.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 # なんとなく、テスト用にいつもこのファイルを使わせてもらってる...
 # デフォルトで使ってるのだけ入手先書いとく。
+# SSD用
 wget https://raw.githubusercontent.com/PINTO0309/MobileNet-SSD-RealSense/master/data/input/testvideo3.mp4
 wget https://raw.githubusercontent.com/PINTO0309/MobileNet-SSD-RealSense/master/data/input/testvideo2.mp4
 
+# 顔検出用
+wget https://raw.githubusercontent.com/intel-iot-devkit/sample-videos/master/face-demographics-walking-and-pause.mp4
+wget https://raw.githubusercontent.com/intel-iot-devkit/sample-videos/master/head-pose-face-detection-female-and-male.mp4
+
+wget -O people.jpg https://s3-ap-northeast-1.amazonaws.com/i.schoo/images/biz/column/845.jpg 
+wget -O group.jpg https://gakumado.mynavi.jp/system/production/image/image/000/144/227/144227/wysiwyg_f0d42500c992ff49ad76.jpg
